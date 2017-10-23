c79b5eb6e745d7e6c0a666572c8ce3b27706a128
pedroclericuzi <pvbc@cin.ufpe.br>
2017-10-23 12:11:58 -0300

    Inserindo uma pasta com o projeto java class

---
+++ algo/FilaBanco/src/index.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class index {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		System.out.println("Por favor, insira seu nome");
+		String nome = in.next();
+		System.out.println("Agora, insira sua idade");
+		int idade = in.nextInt();
+	}
+}
