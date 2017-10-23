c3687763474b43e364a257bc8ac2ca7dc4b2f3d4
pedroclericuzi <pvbc@cin.ufpe.br>
2017-10-23 13:13:16 -0300

    Fazendo a base do projeto

--- algo/FilaBanco/src/index.java
+++ algo/FilaBanco/src/index.java
@@ -5,21 +5,40 @@ public class index {
 	static Scanner in = new Scanner(System.in);
 	public static void main(String[] args){
 		ArrayList<Customer> listCustomer = new ArrayList<>();
+		boolean rodando = true;
 		
-		System.out.println("Escolha uma operação: ");
-		System.out.println("1. Adicionar pessoa;");
-		System.out.println("2. Remover pessoa");
-		int op = in.nextInt();
-		if(op == 1){
-			listCustomer = adicionandoPessoa(listCustomer);
-			for (int i = 0; i < listCustomer.size(); i++) {
-				String nome = listCustomer.get(i).getNome();
-				int idade = listCustomer.get(i).getIdade();
-				System.out.println(nome +", "+idade+" anos");
+		while(rodando){
+			System.out.println("Escolha uma operação: ");
+			System.out.println("1. Adicionar pessoa;");
+			System.out.println("2. Remover pessoa");
+			int op = in.nextInt();
+			if(op == 1){
+				listCustomer = adicionandoPessoa(listCustomer);
+				for (int i = 0; i < listCustomer.size(); i++) {
+					String nome = listCustomer.get(i).getNome();
+					int idade = listCustomer.get(i).getIdade();
+					System.out.println(nome +", "+idade+" anos");
+				}
+			} else {
+				removendoPessoa(listCustomer);
 			}
-		} else {
-			removendoPessoa(listCustomer);
+			
+			System.out.println("Deseja continuar?");
+			System.out.println("1. Sim;");
+			System.out.println("2. Não");
+			int continua = in.nextInt();
+			if(continua==2){
+				rodando = false;
+			} else {
+				for (int i = 0; i < listCustomer.size(); i++) {
+					String nome = listCustomer.get(i).getNome();
+					int idade = listCustomer.get(i).getIdade();
+					System.out.println(nome +", "+idade+" anos");
+				}
+			}
 		}
+		
+		
 		//teste
 	}
 	
